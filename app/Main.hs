e111a5f947e8596ac3a5285fa7df815af8971812
Charles Taylor <charles.taylor@collegevine.com>
2022-04-07 19:53:05 -0400

    Set architecture

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import Data.List (foldl')
+
+allTrue :: [Bool]
+allTrue = True : allTrue
 
 main :: IO ()
-main = someFunc
+main = do
+  let
+    bools :: [Bool]
+    bools = False : allTrue
+
+  putStrLn "This example compiles and terminates"
+  print $ foldr (&&) True bools
+
+  putStrLn "This example compiles but will hang forever"
+  print $ foldl' (&&) True bools
